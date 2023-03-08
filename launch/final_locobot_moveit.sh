53326de2ba229c3d251d48e75c7a087347310003
mbmassey <mbmassey@stanford.edu>
2023-03-08 12:07:51 -0800

    Added executables that run roslaunch with the correct args

---
+++ launch/final_locobot_moveit.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+gnome-terminal -x roslaunch interbotix_xslocobot_moveit xslocobot_moveit.launch robot_model:=locobot_wx250s show_lidar:=true use_actual:=false use_camera:=true use_gazebo:=false dof:=6 use_moveit_rviz:=false
