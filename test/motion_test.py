816bbc3b7c2e434c158af65b32ac1edb6c901803
mbmassey <mbmassey@stanford.edu>
2023-03-08 14:50:28 -0800

    Added interbotix command option to unit testing

--- test/motion_test.py
+++ test/motion_test.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
 # Make sure a locobot works by testing every essential maneuver
+# Set run_on_robot to true if running on real locobot
+# Set use_interbotix to true if using interbotix object instead of moveit object
 
 import time
 import rospy
@@ -11,10 +13,12 @@ from nav_msgs.msg import Odometry
 from geometry_msgs.msg import PoseStamped
 from locobot_motion_example import OrientCamera, MoveLocobotArm
 import moveit_commander
-#from interbotix_xs_modules.locobot import InterbotixLocobotXS
+from interbotix_xs_modules.locobot import InterbotixLocobotXS
 
 class Motion(object):
-    def __init__(self, run_on_robot = False):
+    def __init__(self, run_on_robot = False, use_interbotix = False):
+        self.run_on_robot = run_on_robot
+        self.use_interbotix = use_interbotix
         self.x = 0
         self.y = 0
         self.max_ang_vel = 0.3
@@ -22,9 +26,13 @@ class Motion(object):
         self.motion_time = 2 #seconds
 
         self.drive_controller = DriveController(run_on_robot = run_on_robot)
+        
+        if use_interbotix == True:
+            self.locobot = InterbotixLocobotXS(robot_model="locobot_wx250s", arm_model="mobile_wx250s", use_move_base_action=True)
+
         self.orient_camera = OrientCamera()
         moveit_commander.roscpp_initialize(sys.argv)
-        self.move_locobot_arm = MoveLocobotArm(moveit_commander)
+        self.moveit = MoveLocobotArm(moveit_commander)
         
         if(self.run_on_robot):
             rospy.Subscriber("/camera_frame/mavros/vision_pose/pose", PoseStamped, self.OdometryCallback)
@@ -72,19 +80,38 @@ class Motion(object):
 
     def open_gripper(self):
         print("Opening Gripper")
-        self.move_locobot_arm.open_gripper()
+        if(self.use_interbotix == True):
+            self.locobot.gripper.open()
+        else: 
+            self.moveit.open_gripper()
     
     def close_gripper(self):
         print("Closing Gripper")
-        self.move_locobot_arm.close_gripper()
+        if(self.use_interbotix == True):
+            self.locobot.gripper.close()
+        else: 
+            self.moveit.close_gripper()
     
     def waiting_arm_state(self):
         print("Moving arm to wait under camera")
-        self.move_locobot_arm.move_arm_down_for_camera()
-
+        if(self.use_interbotix == True):
+            self.locobot.arm.go_to_home_pose()
+        else: 
+            self.moveit.move_arm_down_for_camera()
+            
     def grasping_arm_state(self):
         print("Moving arm to grasp")
-        self.move_locobot_arm.move_gripper_down_to_grasp()
+        self.moveit.move_gripper_down_to_grasp()
+
+    def set_arm_to(self, x, z):
+        if(self.use_interbotix == True):
+            print("Moving arm to exact position (x=", x, "z=", z, ")")
+            self.locobot.arm.set_ee_cartesian_trajectory(x, z)
+    
+    def sleeping_arm_state(self, x, z):
+        if(self.use_interbotix == True):
+            print("Moving arm to sleep")
+            self.locobot.arm.go_to_sleep_pose()
 
     def tilt_camera(self):
         print("Tilting camera")
@@ -92,10 +119,13 @@ class Motion(object):
     
 
 if __name__ == "__main__":
-    arg = False
+    arg_1 = False
+    arg_2 = False
     if (sys.argv[1] == "true"):
-        arg = True
-    motion = Motion(run_on_robot=arg)
+        arg_1 = True
+    if (sys.argv[2] == "true"):
+        arg_2 = True
+    motion = Motion(run_on_robot=arg_1, use_interbotix=arg_2)
 
     print("Begin Test sequence: ")
     motion.drive_forward()
@@ -108,3 +138,6 @@ if __name__ == "__main__":
     motion.close_gripper()
     motion.waiting_arm_state()
     motion.tilt_camera()
+    motion.set_arm_to(x=0.1, z=0.25)
+    motion.sleeping_arm_state()
+
